e4787c1b02f5fbeedc4f9600c2f4bc4084f9d4aa
ASSESSORIA30 <olivie@assessoria30.com>
2026-04-07 14:38:02 +0200

    Add CORS options handling to server

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type']
 }));
+
+app.options('*', cors());
 app.use(express.json({ limit: '15mb' }));
 
 app.get('/', (req, res) => {
